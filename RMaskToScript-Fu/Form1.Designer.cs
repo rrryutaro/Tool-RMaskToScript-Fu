8869945715fe63c13d2e8d8cf4a8290ca7370c0c
R <rrryutaro@gmail.com>
2017-05-28 08:21:01 +0900

    v1.0.0.0
    新規作成

---
+++ RMaskToScript-Fu/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace RMaskToScript_Fu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(584, 19);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "(gimp-rect-select img {X} {Y} {Width} {Height} ADD FALSE 0)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(0, 19);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(584, 521);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox2_DragDrop);
+            this.textBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox2_DragEnter);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 540);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(584, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 562);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "RMaskToScript-Fu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ RMaskToScript-Fu/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RMaskToScript_Fu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.AllowDrop = true;
+        }
+
+        private void textBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void textBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+                return;
+
+            string format = textBox1.Text.Replace("{X}", "{0}").Replace("{Y}", "{1}").Replace("{Width}", "{2}").Replace("{Height}", "{3}") + "\r\n";
+
+            foreach (var filePath in (string[])e.Data.GetData(DataFormats.FileDrop))
+            {
+                RectList list = MaskToRect(filePath);
+                foreach (Rect rect in list)
+                {
+                    textBox2.Text += string.Format(format, rect.X, rect.Y, rect.Width, rect.Height);
+                }
+            }
+        }
+
+        private RectList MaskToRect(string filePath)
+        {
+            Bitmap bmp = new Bitmap(filePath);
+            RectList list = new RectList();
+
+            toolStripProgressBar1.Maximum = bmp.Size.Height;
+            toolStripProgressBar1.Value = 0;
+
+            for (int y = 0; y < bmp.Size.Height; y++)
+            {
+                for (int x = 0; x < bmp.Size.Width; x++)
+                {
+                    if (list.Contains(x, y))
+                        continue;
+
+                    Color c = bmp.GetPixel(x, y);
+                    if (c.R == 0 && c.G == 0 && c.B == 0)
+                    {
+                        Color cx;
+                        Color cy;
+                        int x_max;
+                        int y_max = bmp.Size.Height;
+                        int y2;
+
+                        for (x_max = x; x_max < bmp.Size.Width - 1; x_max++)
+                        {
+                            for (y2 = y; y2 < bmp.Size.Height - 1; y2++)
+                            {
+                                cy = bmp.GetPixel(x_max, y2 + 1);
+                                if (cy.R != 0 || cy.G != 0 || cy.B != 0)
+                                    break;
+                            }
+                            if (y2 < y_max)
+                                y_max = y2;
+
+                            cx = bmp.GetPixel(x_max + 1, y);
+                            if (cx.R != 0 || cx.G != 0 || cx.B != 0)
+                                break;
+                        }
+
+                        list.Add(new Rect(x, y, x_max, y_max));
+                    }
+                }
+
+                toolStripProgressBar1.Value++;
+            }
+
+            return list;
+        }
+
+        public class Rect
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+            public int X
+            {
+                get
+                {
+                    return Left;
+                }
+            }
+            public int Y
+            {
+                get
+                {
+                    return Top;
+                }
+            }
+            public int Width
+            {
+                get
+                {
+                    int result = Right - Left + 1;
+                    return result;
+                }
+            }
+            public int Height
+            {
+                get
+                {
+                    int result = Bottom - Top + 1;
+                    return result;
+                }
+            }
+            public Rect(int left, int top, int right, int bottom)
+            {
+                Left = left;
+                Top = top;
+                Right = right;
+                Bottom= bottom;
+            }
+            public bool Contains(int x, int y)
+            {
+                bool result = Left <= x && x <= Right && Top <= y && y <= Bottom;
+                return result;
+            }
+        }
+
+        public class RectList : List<Rect>
+        {
+            public bool Contains(int x, int y)
+            {
+                bool result = false;
+                foreach (Rect rect in this)
+                {
+                    result = rect.Contains(x, y);
+                    if (result)
+                        break;
+                }
+                return result;
+            }
+        }
+    }
+}
